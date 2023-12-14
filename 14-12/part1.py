1259490a1ec6359de31198d6616a68afbdea7a09
Jonas Lütolf <jonas.luetolf@outlook.com>
2023-12-14 10:27:38 +0100

    add 14-12 part1

---
+++ 14-12/part1.py
@@ -0,0 +1,21 @@
+from helper import *
+
+inp = read_inp("input.txt")
+inp = list(map(list,inp))
+inp=list(map(lambda x: "".join(x),transpose(inp)))
+
+
+res = 0
+def _sort(line,top):
+    lines = line.split("#")
+    lines = list(map(lambda x: "".join(sorted(x,reverse=top)),lines))
+    return "#".join(lines)
+
+inp = list(map(lambda x: _sort(x,True),inp))
+inp=transpose(inp)
+
+res = 0
+for i,line in enumerate(inp):
+    res += (len(inp)-i)*line.count("O")
+
+print(res)
